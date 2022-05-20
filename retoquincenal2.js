8f5b0a5cd193ac7b892150001e037492a0270a17
federicojoaquincosta <93017131+federicojoaquincosta@users.noreply.github.com>
2022-05-19 22:57:05 -0300

    Add files via upload

--- retoquincenal2.js
+++ retoquincenal2.js
@@ -1,6 +1,26 @@
 const OpcionesTotales = 20;
-let numeroASaltear = 0; 
+let numeroASaltear = 0;
 
-function saltear(){
-    
+for (let i = 1; i <= OpcionesTotales; i++) {
+        document.querySelector("#checkboxes").innerHTML +=
+                `
+        <label for="${i}"><input type="checkbox" name="" id="${i}">${i}</label>
+        <br>
+        `
+}
+
+
+
+
+
+function saltear() {
+
+        numeroASaltear = document.querySelector("#numeroASaltar")
+
+        for (let i = 0; i <= OpcionesTotales; i++) {
+
+                if ((i % numeroASaltear) == 0) {
+                        
+                }
+        }
 }
