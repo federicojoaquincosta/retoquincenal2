fc2e0458c5316ccf85d37e9507cbdb223f09d50f
federicojoaquincosta <93017131+federicojoaquincosta@users.noreply.github.com>
2022-05-29 12:38:29 -0300

    "v1.0"

--- retoquincenal2.js
+++ retoquincenal2.js
@@ -2,53 +2,44 @@ const OPC_TOTALES = 20;
 let numeroASaltear = 0;
 
 for (let i = 1; i <= OPC_TOTALES; i++) {
-    document.querySelector("#checkboxes").innerHTML +=
-        `
-        <label for="${i}"><input type="checkbox" name="" id="${i}">${i}</label>
+        document.querySelector("#checkboxes").innerHTML +=
+                `
+        <div id="checkbox_${i}"><input type = "checkbox" value="${i}" >Opción ${i}</div>
         <br>
-        `
+        ` ;
 }
-
-
-
 
 
 function saltear() {
-    numeroASaltear = Number(document.querySelector("#numeroASaltear").value)
+        document.querySelector("#mensaje_error").innerHTML = ``;
+        numeroASaltear = Number(document.querySelector("#numeroASaltear").value);
 
-    document.querySelector("#checkboxes").innerHTML = ``;
 
-    for (let i = numeroASaltear; i <= OPC_TOTALES; i += numeroASaltear) {
+        for (let i = 1; i <= OPC_TOTALES; i++) {
+                document.querySelector(`#checkbox_${i}`).innerHTML = `
+             
+            <input type = "checkbox" value="1" >Opción ${i}
+        `;
 
-        console.log("valor de i:", { i });
-        document.querySelector(`#checkboxes`).innerHTML +=
-            `
-        <label for="${i}"><input checked=checked type="checkbox" name="" id="${i}">${i}</label>
-        <br>
-        `
-        
+        }
 
-    }
+        if (numeroASaltear > 0 && numeroASaltear < 21) {
+                for (i = numeroASaltear; i < 21; i += numeroASaltear) {
+
+
+                        if (i >= 1 || i < 20) {
+                                document.querySelector(`#checkbox_${i}`).innerHTML = `
+                        <input type = "checkbox" value="1" checked >Opción ${i}
+                        `;
+                        }
+
+                }
+        }
+        else {
+                document.querySelector("#mensaje_error").innerHTML = `Ingresá un número del 1 al 20`;
+
+
+        }
 }
 
 
-
-
-
-
-/*for (let i = numeroASaltear; i <= OpcionesTotales; i++) {
-    if (i == numeroASaltear) {
-        document.querySelector("#checkboxes").innerHTML +=
-        `
-        <label for="${i}"><input checked=checked type="checkbox" name="" id="${i}">${i}</label>
-        <br>
-        `
-        
-    } else if (i != numeroASaltear) {
-    document.querySelector("#checkboxes").innerHTML +=
-        `
-        <label for="${i}"><input type="checkbox" name="" id="${i}">${i}</label>
-        <br>
-        `
-    }
-}*/
